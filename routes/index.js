c6f8e4a461f0b50ad726161bbdf5d4fc24068c16
Oscar Martinez <oscar_noa@hotmail.com>
2015-07-05 14:36:47 +0200

    AutoLoad
    Agrego autoload

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,9 @@ router.get('/author', function(req, res) {
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer)
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
