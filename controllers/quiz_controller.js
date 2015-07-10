02e78ec714fdfaa69ab3157c423a32bb49dd24d9
Oscar Martinez <oscar_noa@hotmail.com>
2015-07-10 18:51:18 +0200

    Crear comentario
    Se añade logica para los comentarios, funciona sqlite...

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -4,8 +4,14 @@ var temas = ["Otro", "Humanidades", "Ocio", "Ciencia", "Tecnología" ];
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.find(quizId).then(
-    function(quiz) {
+    models.Quiz.find({
+            where: {
+                id: Number(quizId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(function(quiz) {
       if (quiz) {
         req.quiz = quiz;
         next();
