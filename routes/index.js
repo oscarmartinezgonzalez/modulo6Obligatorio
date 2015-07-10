cf7147a3d8ca4bbb025f8c201e3d25404ea3eeb2
Oscar Martinez <oscar_noa@hotmail.com>
2015-07-10 19:22:41 +0200

    Moderacion de comentarios
    Se añade la opcion de publicar los comentarios por parte de usuario
    registrado, si es anonimo no se mostrará hasta que se valide

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -36,6 +37,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 
 
