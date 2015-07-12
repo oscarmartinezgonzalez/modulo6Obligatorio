11fbcf9867cb69766e7e8569d78f86b3c1001195
Oscar Martinez <oscar_noa@hotmail.com>
2015-07-12 16:02:26 +0200

    Added Blank

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -9,6 +9,6 @@ exports.autenticar = function(login, password, callback) {
         if(password === users[login].password){
             callback(null, users[login]);
         }
-        else { callback(new Error('Password erróneo.')); }
+        else { callback(new Error('Password erróneo. ')); }
     } else { callback(new Error('No existe el usuario.'));}
 };
